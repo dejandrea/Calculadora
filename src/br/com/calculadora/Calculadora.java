c48ac3e0a2bff59e814a31b434a5c742445c7b77
Andréa J S França <dejandrea@gmail.com>
2022-07-10 00:54:43 -0300

    first commit

---
+++ src/br/com/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor:");
+		a = scan.nextInt();
+		System.out.println("Digite o Segundo valor:");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		float divisao = divisao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(divisao);
+		System.out.println(multiplicacao);
+		
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public static float divisao(float a, float b) {
+		return a / b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a + b;
+	}
+
+}
